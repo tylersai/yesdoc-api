633c4ce46caa905c352efa310e4dd6c6f701cee6
Sai Han <thesaihan@gmail.com>
2020-01-13 16:10:31 +0630

    Add mongoose nodemon dependencies

--- app.js
+++ app.js
@@ -2,6 +2,11 @@ const express = require('express');
 const app = express();
 const port = 2000;
 
+const cors = require('cors');
+
+// Middleware
+app.use(cors());
+
 app.get('/', (req, res) => res.send('Hello World!'));
 
 app.listen(port, () => console.log(`Listening on port ${port}!`));
