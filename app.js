9844c4b29a7095eef9e7f67473225a7781f93cf1
Tyler Sai <thesaihan@gmail.com>
2022-12-01 15:17:06 +0700

    Add app info in root path

--- app.js
+++ app.js
@@ -3,11 +3,20 @@ const app = express();
 
 const cors = require("cors");
 const bodyParser = require("body-parser");
-require('dotenv/config');
+require("dotenv/config");
 
 // Middleware
 app.use(cors());
 app.use(bodyParser.json());
+
+// App healthcheck (root)
+app.get("/", (req, res) => {
+  res.json({
+    name: "yesdoc-api",
+    description: "Backend RESTful API for Yes Doctor Employee Benefit Platform using Express framework of NodeJS ",
+    version: "v1.0.0",
+  });
+});
 
 // Router
 const employeeController = require("./controller/EmployeeController");
@@ -17,10 +26,8 @@ app.use("/transaction", transController);
 
 // Database
 const mongoose = require("mongoose");
-mongoose.connect(
-  process.env.CON_LINK,
-  { useNewUrlParser: true, useUnifiedTopology: true },
-  () => console.log("Connected to MongoDB!")
+mongoose.connect(process.env.CON_LINK, { useNewUrlParser: true, useUnifiedTopology: true }, () =>
+  console.log("Connected to MongoDB!")
 );
 
 const port = process.env.PORT;
